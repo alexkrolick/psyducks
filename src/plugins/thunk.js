6ae77132d3fa3f807c3be4c7f630a3671d2cef58
Alex Krolick <alexander.krolick@gmail.com>
2018-04-29 20:35:49 -0700

    Delete unimplemented thunk

--- src/plugins/thunk.js
+++
@@ -1,9 +0,0 @@
-/**
- * Calls functions
- */
-export default function shallowMerge(reducer) {
-  return (state, action) => {
-    const partialUpdate = reducer(state, action)
-    return { ...state, ...partialUpdate }
-  }
-}
